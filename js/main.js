cc318398fb2a66504e51a33636eec2da128869c9
GhotsboyD <18829343482@163.com>
2019-03-11 09:52:19 +0800

    dc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+﻿$(document).ready(function() {
 
   $('a.blog-button').click(function() {
     // If already in blog, return early without animate overlay panel again.

a2362c6df6040f7a8ddd8e2bfc28cb9f79b39ba6
GhotsboyD <18829343482@163.com>
2019-03-11 10:04:47 +0800

    Revert "dc"
    This reverts commit cc318398fb2a66504e51a33636eec2da128869c9.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function() {
+$(document).ready(function() {
 
   $('a.blog-button').click(function() {
     // If already in blog, return early without animate overlay panel again.
